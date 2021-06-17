282a2a553fbab444041da83bdb7f391d2cd55766
PavelKhlebinski <predramer@gamil.com>
2021-06-17 15:04:25 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Gmail API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = "714942312383-gairlb0r7e9mkfgmup2g3o5e1m7tt890.apps.googleusercontent.com",
+                    ClientSecret = "RqG_qqsoqdCX1iZu4AmGQ689"
+                },
+                Scopes,
+                "user",
+                CancellationToken.None,
+                new FileDataStore("Analytics.Auth.Store")).Result;
+
+            // Create Gmail API service.
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            UsersResource.LabelsResource.ListRequest request = service.Users.Labels.List("me");
+
+            // List labels.
+            IList<Label> labels = request.Execute().Labels;
+            Console.WriteLine("Labels:");
+            if (labels != null && labels.Count > 0)
+            {
+                foreach (var labelItem in labels)
+                {
+                    Console.WriteLine("{0}", labelItem.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No labels found.");
+            }
+            Console.Read();
+        }
+    }
+}
